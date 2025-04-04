e16fbe1e621b1babe5ffa6078e23cd77c7f0515d
M Haidar Hanif <git@mhaidarhanif.com>
2025-04-04 14:09:02 +0700

    feat: setup openapi and scalar api reference

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
-import { Hono } from "hono";
+import { apiReference } from "@scalar/hono-api-reference";
 import { adminCitiesRoute } from "./modules/admin-city/route";
 import { citiesRoute } from "./modules/city/route";
 import { commonRoute } from "./modules/common/route";
 import { placesRoute } from "./modules/place/route";
 import { searchRoute } from "./modules/search/route";
+import { OpenAPIHono } from "@hono/zod-openapi";
 
-const app = new Hono();
+const app = new OpenAPIHono();
 
 app.route("/", commonRoute);
 app.route("/cities", citiesRoute);
 app.route("/places", placesRoute);
 app.route("/search", searchRoute);
 app.route("/admin/cities", adminCitiesRoute);
+
+app.doc("/openapi.json", {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "Urbanesia API",
+  },
+});
+
+app.get(
+  "/docs",
+  apiReference({
+    url: "/openapi.json",
+  })
+);
 
 export default app;
